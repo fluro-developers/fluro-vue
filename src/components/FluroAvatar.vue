712b91c4990d476495d1f2b022269919a6d1d96f
Cade Embery <cadeembery@Cades-MacBook-Pro-2.local>
2019-04-25 00:26:30 +1000

    First Commit

---
+++ src/components/FluroAvatar.vue
@@ -0,0 +1,61 @@
+<template>
+
+<div class="avatar">
+	<div class="avatar-inner" :style="{backgroundImage:backgroundImage}"></div>
+</div>
+</template>
+<script>
+
+export default {
+	props:['id', 'type', 'width', 'size'],
+	computed:{
+		// classes(){
+		// 	return size;
+		// },
+		backgroundImage() {
+			
+			return `url("${this.$fluro.asset.avatarUrl(this.id, this.type || 'contact', {w:this.width || 100}) }")`;
+			// return this.$fluro.asset.avatarUrl(this.id, this.type || 'contact', {w:this.width || 100}) ;
+		},
+	}
+}
+</script>
+
+<style scoped lang="scss">
+
+
+.avatar {
+	display:inline-block;
+	width: 24px;
+	vertical-align: middle;
+	margin-right: 10px;
+
+	&.md {
+		width:36px;
+	}
+
+	&.lg {
+		width:48px;
+	}
+
+	&.xl {
+		width:64px;
+	}
+}
+
+.avatar-inner {
+	max-width: 100%;
+	padding-bottom: 100%;
+	position:relative;
+	overflow:hidden;
+	height:0;
+	border-radius: 50%;
+	background-color: rgba(#222,0.1);
+	background-position:center center;
+	background-size: cover;
+	background-repeat: no-repeat;
+
+}
+
+
+</style>
---
+++ src/components/FluroImage.vue
@@ -0,0 +1,198 @@
+<template>
+    <div class="fluro-image" :class="{loaded:loaded}" :style="{backgroundImage:backgroundImage}">
+        <div :style="spacer"></div>
+        <!-- <transition name="fade"> -->
+        <div class="placeholder" v-if="!loaded" :style="{backgroundImage:placeholderImage}"></div>
+        <!-- </transition> -->
+        <v-progress-circular v-if="!loaded && spinner" indeterminate color="rgba(180,180,180,0.5)"></v-progress-circular>
+        <slot></slot>
+    </div>
+</template>
+<script>
+export default {
+    props: {
+        spinner: {
+            type: Boolean
+        },
+        item: [String, Object],
+        width: {
+            type: Number,
+        },
+        height: {
+            type: Number,
+        },
+    },
+    data() {
+        return {
+            loaded: false,
+        }
+    },
+    watch: {
+        'url': 'load',
+    },
+    mounted() {
+        this.load();
+    },
+    methods: {
+        load() {
+
+            console.log('Start Loading')
+            var self = this;
+            self.loaded = false;
+
+            if (!self.url) {
+                return;
+            }
+
+            var url = self.url;
+
+            //Create a new image
+            var img = new Image;
+
+
+            //Once the image has loaded
+            img.onload = function($event) {
+                self.loaded = true;
+                self.$emit('load', $event);
+            }
+
+            //Once the image has loaded
+            img.onerror = function($event) {
+                self.$emit('error', $event);
+            }
+
+            img.src = url;
+        }
+    },
+    computed: {
+        computedWidth() {
+            return _.get(this.item, 'width') || this.width || 0;
+        },
+        computedHeight() {
+            return _.get(this.item, 'height') || this.height || 0;
+        },
+        aspectRatio() {
+            return (this.computedHeight / this.computedWidth * 100);
+        },
+        spacer() {
+            var self = this;
+
+            var style = {}
+
+            if (self.aspectRatio) {
+                style.height = `0`;
+                style.paddingBottom = `${self.aspectRatio}%`;
+            }
+
+            return style;
+        },
+        imageID() {
+            return this.$fluro.utils.getStringID(this.item);
+        },
+        url() {
+            if (this.imageID) {
+
+
+                //Allow the Fluro API to decide the best dimensions based on screensize
+                return this.$fluro.asset.imageUrl(this.imageID, this.width, this.height);
+            } else {
+                return;
+            }
+        },
+        placeholderImage() {
+            if (this.imageID) {
+                var placeholderUrl = this.$fluro.asset.imageUrl(this.imageID, 50);
+                return `url(${placeholderUrl})`;
+            } else {
+                return;
+            }
+        },
+        backgroundImage() {
+
+            if (!this.url) {
+                return;
+            }
+
+            return `url(${this.url})`;
+
+        }
+    }
+}
+</script>
+<style scoped lang="scss">
+.fade-enter-active,
+.fade-leave-active {
+    transition: opacity .8s;
+}
+
+.fade-enter,
+.fade-leave-to {
+    opacity: 0;
+}
+
+
+.fluro-image {
+    overflow: hidden;
+    max-width: 100%;
+    position: relative;
+    // background-color: #eee;
+    background-size: 100% auto;
+    background-repeat: no-repeat;
+    background-position: center center;
+    color: #fff;
+    flex: 1 0 auto;
+
+
+
+    .v-progress-circular {
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        transform: translate3d(-50%, -50%, 0);
+    }
+
+    .spacer {
+        position: relative;
+    }
+
+    .placeholder {
+        position: absolute;
+        left: 0;
+        right: 0;
+        top: 0;
+        bottom: 0;
+        // transition: opacity 0.2s;
+        background-size: 100% auto;
+        background-repeat: no-repeat;
+        background-position: center center;
+        // filter:blur(5px);
+    }
+
+    &[contain] {
+
+        &,
+        .placeholder {
+            background-size: contain;
+        }
+    }
+
+    &[cover] {
+
+        &,
+        .placeholder {
+            background-size: cover;
+        }
+    }
+
+    // &.loaded {
+    //     .placeholder {
+    //         opacity: 0;
+    //     }
+    // }
+
+    img {
+        width: 100%;
+        display: block;
+    }
+}
+</style>
