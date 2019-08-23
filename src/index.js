5f5f1d311417450417f3d2f74bf03fecc0973b13
Cade Embery <cade@hyperblaster.org>
2019-08-23 15:39:09 +1000

    moved peer dependencies

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@
 
 // export default FluroVue;
 
-console.log('fluro-vue 2.0.4')
+console.log('fluro-vue 2.0.5')
 // process.env.VUE_APP_VERSION = require('./package.json').version
 
 // import store from './store'
