b114a37958741dff1aa76a468ec4ec2bbfdd6beb
Cade Embery <cade@hyperblaster.org>
2019-11-25 13:05:15 +1100

    Updates

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@
 
 // export default FluroVue;
 
-console.log('fluro-vue 2.0.21')
+console.log('fluro-vue 2.0.22')
 // process.env.VUE_APP_VERSION = require('./package.json').version
 
 // import store from './store'
