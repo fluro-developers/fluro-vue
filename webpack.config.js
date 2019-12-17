bc88d1de8517279b568cfd6518abd316964145ea
Cade Embery <cade@hyperblaster.org>
2019-12-17 11:35:55 +1100

    Updated globalObject to enable SSR

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,8 @@ module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'index.js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'dist'),
+        globalObject: "this"
     },
     externals: {
         'fluro':'fluro',
