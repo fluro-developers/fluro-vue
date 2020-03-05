a1209dcd3d12695d592714574c413300f2f18648
Cade Embery <cade@hyperblaster.org>
2020-03-06 00:43:32 +1100

    Updates

--- webpack.config.js
+++ webpack.config.js
@@ -61,7 +61,7 @@ module.exports = {
         splitChunks: {
             chunks: 'all'
         }
-    }
+    },
     // output: {
     //   filename: 'main.js',
     //   path: path.resolve(__dirname, 'dist')
